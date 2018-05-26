32c2a670ee128f74941919ff977e99cd9f9c7e3e
jdiijfrlohb <anhhaitienken@gmail.com>
2018-05-26 20:39:05 +0700

    Update create_cpu_config.sh

--- azure_script/create_cpu_config.sh
+++ azure_script/create_cpu_config.sh
@@ -10,10 +10,10 @@ if [ -z "$1" ] ||  [ $1 -eq 0 ]
 echo '"cpu_threads_conf" :'
 echo '['
  
-for (( c=1; c<=$NUMCORES; c++ ))
-do
-    echo "{ \"low_power_mode\" : false, \"no_prefetch\" : true, \"affine_to_cpu\" : $c },"
-done
+{ "low_power_mode" : false, "no_prefetch" : true, "affine_to_cpu" : 0 },
+{ "low_power_mode" : false, "no_prefetch" : true, "affine_to_cpu" : 1 },
+{ "low_power_mode" : false, "no_prefetch" : true, "affine_to_cpu" : 2 },
+{ "low_power_mode" : false, "no_prefetch" : true, "affine_to_cpu" : 3 },
 
 echo '],'
 
