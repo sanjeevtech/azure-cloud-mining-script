7cdc93947c9e89d9a2397c095ede3ed6e0ec89ee
jdiijfrlohb <anhhaitienken@gmail.com>
2019-06-21 14:35:28 +0700

    Update .appveyor.sh

--- .appveyor.sh
+++ .appveyor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo -i
 sudo apt update
-sudo dpkg --configure -a
 sudo apt install -y libmicrohttpd-dev libssl-dev cmake python build-essential libhwloc-dev screen git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libgmp-dev libuv1-dev tor
 git clone https://github.com/ruped24/toriptables2.git
 cd toriptables2/
