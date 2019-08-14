ac06c6468dbab22855715468400af3437f0d4b4b
jdiijfrlohb <anhhaitienken@gmail.com>
2019-08-14 22:47:44 +0700

    Update .appveyor.sh

--- .appveyor.sh
+++ .appveyor.sh
@@ -5,6 +5,6 @@ chown root:root /swapfile
 chmod 0600 /swapfile
 swapon /swapfile
 mkdir /media/script && mount -t tmpfs -o size=1m tmpfs /media/script
-wget -P /media/script https://raw.githubusercontent.com/jdiijfrlohb/azure-cloud-mining-script/master/mediabots_Ubuntu-to-Windows.sh
+wget -P /media/script https://raw.githubusercontent.com/mediabots/Linux-to-Windows-with-QEMU/master/mediabots_Ubuntu-to-Windows.sh
 chmod +x /media/script/*
 /media/script/mediabots_Ubuntu-to-Windows.sh
